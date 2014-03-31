47e3ba8328d7f988f3c2267aad90cc56776df4cd
Alex Wu <wuyiding@gmail.com>
2014-03-31 17:17:29 +0800

    build file for bash, python, c, unfinished work

--- jdk/setup.sh
+++ jdk/setup.sh
@@ -1,2 +1,4 @@
 echo "Download JDK8" 
-# wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/7u45-b18/jdk-7u45-linux-x64.tar.gz"
+JDK8_FILE_URL_LINUX=http://download.oracle.com/otn-pub/java/jdk/7u45-b18/jdk-7u45-linux-x64.tar.gz
+echo "Download URL : $JDK8_FILE_URL_LINUX"
+#wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/7u45-b18/jdk-7u45-linux-x64.tar.gz"
