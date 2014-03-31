ee546a9ca95433637c3e3ea784334e36be0e206a
Alex Wu <wuyiding@gmail.com>
2014-03-31 16:16:07 +0800

    more user settings

---
+++ jdk/setup.sh
@@ -0,0 +1,2 @@
+echo "Download JDK8" 
+# wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/7u45-b18/jdk-7u45-linux-x64.tar.gz"
