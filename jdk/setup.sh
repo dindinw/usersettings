f075b2d375dec7fdd6108203c0ca1710b44dc6f8
alex wu <wuyiding@gmail.com>
2014-04-03 21:36:32 +0800

    tested for linux (ubuntu), unfinish for mac and win

--- jdk/setup.sh
+++ jdk/setup.sh
@@ -1,30 +1,83 @@
+# Links
+
+# JDK 7
+JDK7_FILE_URL_LINUX_64=http://download.oracle.com/otn-pub/java/jdk/7u45-b18/jdk-7u45-linux-x64.tar.gz
+JDK7_FILE_URL_WIN_64=http://download.oracle.com/otn-pub/java/jdk/7u45-b18/jdk-7u45-windows-x64.exe
+
+# JDK 8
+JDK8_NAME=jdk1.8.0    
+# Linux 
+JDK8_FILE_URL_LINUX_32=http://download.oracle.com/otn-pub/java/jdk/8-b132/jdk-8-linux-i586.tar.gz
+JDK8_FILE_CHECKSUM_LINUX_32=45556e463a561b470bd9d0c07a73effb
+
+JDK8_FILE_URL_LINUX_64=http://download.oracle.com/otn-pub/java/jdk/8-b132/jdk-8-linux-x64.tar.gz
+JDK8_FILE_CHECKSUM_LINUX_64=7e9e5e5229c6603a4d8476050bbd98b1
+
+# Mac    
+JDK8_FILE_URL_MAC_64=http://download.oracle.com/otn-pub/java/jdk/8-b132/jdk-8-macosx-x64.dmg
+
+# Windows
+JDK8_FILE_URL_WIN_64=http://download.oracle.com/otn-pub/java/jdk/8-b132/jdk-8-windows-x64.exe
+
+# File Extension
+FILE_EXT_WIN=.exe
+FILE_EXT_LINUX=.tar.gz
+FILE_EXT_MAC=.dmg
+
+# Default Download Place
+DOWNLOAD_DIR=
+
+# Install Place
+INSTALL_BASE=~/_devtools
+JDK_INSTALL_BASE=$INSTALL_BASE/java/jdk
+
 # Globle Var
+OS=
+MACH=
+FILE_EXT=
 DOWNLOAD_URL=
 JDK_FILE=
+JDK_FILE_CHECKSUM=
+JDK_NAME=
 
-function prepareByPlatform()
+FORCE_DOWNLOAD=false
+
+function preparePlatform()
 {
-    OS=$(uname -s)
+    OS=$(uname)
+    MACH=$(uname -m)
     echo "OS Platfrom : $OS"
-
-    JDK7_FILE_URL_LINUX_64=http://download.oracle.com/otn-pub/java/jdk/7u45-b18/jdk-7u45-linux-x64.tar.gz
-    JDK7_FILE_URL_WIN_64=http://download.oracle.com/otn-pub/java/jdk/7u45-b18/jdk-7u45-windows-x64.exe
+    echo "    Machine : $MACH"
     
-    JDK8_FILE_URL_LINUX_64=http://download.oracle.com/otn-pub/java/jdk/8-b132/jdk-8-linux-x64.tar.gz
-    JDK8_FILE_URL_MAC_64=http://download.oracle.com/otn-pub/java/jdk/8-b132/jdk-8-macosx-x64.dmg
-    JDK8_FILE_URL_WIN_64=http://download.oracle.com/otn-pub/java/jdk/8-b132/jdk-8-windows-x64.exe
-    
-
     if [[ "$OS" == "Linux" ]]; then
-        JDK8_FILE_URL_LINUX=$JDK8_FILE_URL_LINUX_64
+        FILE_EXT=$FILE_EXT_LINUX
+        DOWNLOAD_DIR=~/Downloads
+        if [[ "$MACH" == "x86_64" ]]; then
+            #64 bit
+            JDK8_FILE_URL_LINUX=$JDK8_FILE_URL_LINUX_64
+            JDK_FILE_CHECKSUM=$JDK8_FILE_CHECKSUM_LINUX_64
+        else
+            #32 bit
+            JDK8_FILE_URL_LINUX=$JDK8_FILE_URL_LINUX_32
+            JDK_FILE_CHECKSUM=$JDK8_FILE_CHECKSUM_LINUX_32
+        fi
         echo "JDK8 Linux URL : $JDK8_FILE_URL_LINUX"
         DOWNLOAD_URL=$JDK8_FILE_URL_LINUX
+        JDK_NAME=$JDK8_NAME
 
-    elif [[ "$OS" == "Mac" ]]; then
+    elif [[ "$OS" == "Darwin" ]]; then
+        OS="Mac"
+        DOWNLOAD_DIR=~/Downloads
+
         echo "JDK8 Mac URL : $JDK8_FILE_URL_MAC"
         #TODO
 
     else #WIN
+        OS="Win"
+        DOWNLOAD_DIR=~/Downloads 
+
+        #TODO CheckSUM
+        
         JDK8_FILE_URL_WIN=$JDK8_FILE_URL_WIN_64
         echo "JDK8 Win URL : $JDK8_FILE_URL_WIN"
         DOWNLOAD_URL=$JDK8_FILE_URL_WIN
@@ -39,34 +92,93 @@ function callWget()
     wget --no-cookies --no-check-certificate \
         --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" \
         "$DOWNLOAD_URL" \
-        -O $JDK_FILE
+        -O $DOWNLOAD_DIR/$JDK_FILE
 }
+
+function checkMD5SUM_Linux()
+{
+    local result=$(md5sum $DOWNLOAD_DIR/$JDK_FILE| awk '{ print $1 }')
+    echo $result 
+}
+
+function checkMD5SUM_Mac()
+{
+    local result=$(md5 -q $DOWNLOAD_DIR/$JDK_FILE)
+    echo $result
+}
+
+function checkMD5SUM_Win()
+{
+    checkMD5SUM_Linux
+}
+
 
 function downlaodJDK()
 {
     echo "Download JDK" 
-    echo "Download JDK8" 
-    callWget $DOWNLOAD_URL
-}
-function installJDK()
-{
-    #TODO (How to pass in the /s option)
-    echo "Install $JDK_FILE..."
-    wait $(./$JDK_FILE //s)
+    echo "Download JDK8"
+    # echo checkMD5SUM_$OS $JDK_FILE_CHECKSUM
+    if [[ "$FORCE_DOWNLOAD" == true ]] || [[ ! -e $DOWNLOAD_DIR/$JDK_FILE ]] || [[ ! "$(checkMD5SUM_$OS)" == "$JDK_FILE_CHECKSUM" ]]; then
+        echo callWget
+    fi
 }
 
-function setupJDKEnv()
+function installJDK_Win()
+{
+    #TODO (How to pass in the /s option)
+    wait $($DOWNLOAD_DIR/$JDK_FILE //s)
+}
+
+function installJDK_Linux()
+{
+    echo "extracing $JDK_FILE..."
+    if [ ! -d $INSTALL_BASE ]; then mkdir -m0755 -p $INSTALL_BASE; fi
+    if [ ! -d $JDK_INSTALL_BASE ]; then mkdir -m0755 -p $JDK_INSTALL_BASE; fi
+    chmod -R 755 $INSTALL_BASE
+    if [ ! -d $JDK_INSTALL_BASE/$JDK_NAME ]; then 
+        wait $(tar xzf $DOWNLOAD_DIR/$JDK_FILE -C $JDK_INSTALL_BASE)
+    fi
+    if [ -d $JDK_INSTALL_BASE/$JDK_NAME ]; then echo done!; fi
+}
+
+function installJDK_Mac()
+{
+    echo "...TODO installJDK_Mac"
+}
+
+function setupJDKEnv_Linux()
+{
+    eval $(echo export JAVA_HOME=$JDK_INSTALL_BASE/$JDK_NAME)
+    eval $(echo export PATH=$JAVA_HOME/bin:$PATH)
+    printenv|grep JAVA_HOME
+    printenv|grep ^PATH
+    java -version
+    
+}
+function setEnv(){
+    echo export JAVA_HOME=BAR
+}
+
+function setupJDKEnv_Mac()
 {
     #TODO
-    echo test
+    echo "...TODO setupJDKEnv_Mac"
+}
+
+function setupJDKEnv_Win()
+{
+    #TODO
+    echo "...TODO setupJDKEnv_Win"
 }
 
 function main()
 {
-    prepareByPlatform
-    #downlaodJDK
-    installJDK
-    setupJDKEnv
+    preparePlatform
+    downlaodJDK
+    echo "Install $JDK_FILE ..."
+    installJDK_$OS
+    echo "Setup $JDK_NAME ..."
+    setupJDKEnv_$OS
 }
 
 main
