b5c7a0ddd6fd7988b0beb8ada4733966f5e02614
Alex Wu <wuyiding@gmail.com>
2014-04-04 15:50:17 +0800

    function for window platfrom

--- lib/func_win.sh
+++ lib/func_win.sh
@@ -1,18 +1,63 @@
 uvar="HKCU\Environment"
 mvar="HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment"
 
-function setUserEnv(){
-
+function getAllUserEnv(){
+    reg query "$uvar"
+}
+function getAllSysEnv(){
+    reg query "$mvar"
 }
 
-function getUserEnvByKey(){
+function queryRegByVarAndKey(){
+    local var=$1
+    local key=$2
+    local result=$(reg query "$var" //v "$key" 2>&1)
+    echo $result |grep $key 2>&1 > /dev/null
+    if [[ $? -eq 0 ]]; then
+        echo $result |grep $key | sed -e "s/^.*$key.*REG.*SZ\s//"
+        return 0
+    fi
+    return -1
+}
 
+function getUserEnv(){
+    local key=$1
+    queryRegByVarAndKey $uvar $key
+    return $?
+
+}
+function getSysEnv(){
+    local key=$1
+    queryRegByVarAndKey "$mvar" "$key"
+    return $?
+}
+
+function setUserEnv(){
+    local key=$1
+    local value=$2
+    setx $key $value 2>&1 > /dev/null
+}
+
+################################################################################
+# name : delUserEnv
+# desc : del user's enviroment value by given key
+#Usage : delUserEnv "FOO"
+################################################################################
+function delUserEnv(){
+    local key=$1
+    local result=$(queryRegByVarAndKey "$key")
+    if [[ -n $result ]]; then 
+        reg delete "$uvar" //v "$key" //f 2>&1 > /dev/null
+    fi
 }
 
 function setSysEnv(){
+    echo "...TODO"
+
+}
+function getSysEnv(){
+    echo "...TODO"
 
 }
 
-function getSysEnvByKey(){
 
-}
--- lib/test/test_func_win.sh
+++ lib/test/test_func_win.sh
@@ -1,1 +1,39 @@
 source ../func_win.sh
+
+echo "========================================================================="
+echo "Test getAllUserEnv..."
+echo "========================================================================="
+getAllUserEnv
+
+echo
+echo "========================================================================="
+echo "Test getAllSysEnv..."
+echo "========================================================================="
+getAllSysEnv
+
+echo
+echo "========================================================================="
+echo "Test getUserEnv..."
+echo "========================================================================="
+getUserEnv "JAVA_HOM"
+echo execute return value is $?
+getUserEnv "JAVA_HOME"
+echo execute return value is $?
+JAVA_HOME=$(getUserEnv "JAVA_HOME")
+echo JAVAHOME=$JAVA_HOME
+
+echo 
+echo "========================================================================="
+echo "Test getSysEnv..."
+echo "========================================================================="
+MYPATH=$(getSysEnv "PATH")
+echo MYPATH=$PATH
+echo
+
+echo "========================================================================="
+echo "Test setUserEnv and delUserEnv..."
+echo "========================================================================="
+setUserEnv "FOO" "BAR"
+getUserEnv "FOO"
+delUserEnv "FOO"
+getUserEnv "FOO"
