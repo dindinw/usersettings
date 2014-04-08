0eaa23b52ff3c041c78da00a06edf5da8e03d52f
Alex Wu <wuyiding@gmail.com>
2014-04-08 10:16:18 +0800

    dos version to query amazon page for ebook metadata. ugly!

--- ebook/rename.bat
+++ ebook/rename.bat
@@ -1,17 +1,41 @@
 @rem
-call :amazonIsdn 111879429X
-call :amazonIsdn 0321942051
-call :amazonIsdn 012408138X
-goto:eof
+@call :amazonIsdn 111879429X
+@call :amazonIsdn 0321942051
+@call :amazonIsdn 012408138X
+@goto:eof
 
 :amazonIsdn
 @echo input ISDN is %~1
 @set isdn=%~1
-@curl -s -L http://www.amazon.com/dp/%isdn% | findstr /R "btAsinTitle" 2>NUL 1>tmpFile
-@rem type tmpFile
-@for /f "tokens=1 delims=<" %%a in ('type tmpFile') do @set title="%%a"
+@curl -s -L http://www.amazon.com/dp/%isdn% 1>tmpFile
+
+@for /f "tokens=1 delims="  %%a in ('findstr /R "btAsinTitle" tmpFile') do @set title="%%a"
 @rem echo %title%
 @for /f "tokens=2 delims=>" %%b in ('echo %title%') do @set title="%%b"
-@echo Title : %title:~,-1%
+@rem echo %title%
+@for /f "tokens=1 delims=<" %%c in ('echo %title%') do @set title="%%c"
+
+
+@for /f "tokens=1 delims=" %%d in ('findstr /R "Publication.*Date.*ISBN-10.*%isbn%.*Edition.*" tmpFile') do @set details="%%d"
+@rem echo Details : %details%
+
+@for /f "tokens=1 delims=|" %%e in ('echo %details%') do @set publishdate='%%e'
+@for /f "tokens=2 delims=|" %%f in ('echo %details%') do @set isbn-10=%%f
+@for /f "tokens=3 delims=|" %%f in ('echo %details%') do @set isbn-13=%%f
+@for /f "tokens=4 delims=|" %%f in ('echo %details%') do @set edition="%%f"
+
+
+
+@set title=%title:~2,-1%
+@set publishdate=%publishdate:~105,-35%
+@set isbn-10=%isbn-10:~50,-35%
+@set isbn-13=%isbn-13:~50,-35%
+@set edition=%edition:~51,-17%
+@echo Title   : %title%
+@echo Publish : %publishdate%
+@echo ISBN-10 : %isbn-10%
+@echo ISBN-13 : %isbn-13%
+@echo Edition : %edition%
+
 @del tmpFile
 
