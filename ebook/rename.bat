d1994807bf886201239d1ac7bf591caf7b9c95c8
Alex Wu <wuyiding@gmail.com>
2014-04-09 09:53:54 +0800

    improve for dos batch for renaming ebook

--- ebook/rename.bat
+++ ebook/rename.bat
@@ -2,6 +2,7 @@
 @call :amazonIsdn 1626710104
 @call :amazonIsdn 0321942051
 @call :amazonIsdn 012408138X
+@call :amazonIsdn B00BFWI6CG
 @goto:eof
 
 :amazonIsdn
@@ -9,12 +10,18 @@
 @set isdn=%~1
 @curl -s -L http://www.amazon.com/dp/%isdn% 1>tmpFile
 
+@rem do check first
+
+@findstr /R "btAsinTitle" tmpFile > tempfindoutput
+@rem Notice 'findstr /R /N "^" tempfindoutput ^| find /C ":"' is count of fileline.
+@for /f %%i in ('findstr /R /N "^" tempfindoutput ^| find /C ":"') do @set FINDOUTPUT=%%i 
+@IF %FINDOUTPUT%==0 @goto:notFoundExit
+
 @for /f "tokens=1 delims="  %%a in ('findstr /R "btAsinTitle" tmpFile') do @set title="%%a"
 @rem echo %title%
 @for /f "tokens=2 delims=>" %%b in ('echo %title%') do @set title="%%b"
 @rem echo %title%
 @for /f "tokens=1 delims=<" %%c in ('echo %title%') do @set title="%%c"
-
 
 @for /f "tokens=1 delims=" %%d in ('findstr /R "Publication.*Date.*ISBN-10.*%isbn%.*Edition.*" tmpFile') do @set details="%%d"
 @rem echo Details : %details%
@@ -23,8 +30,6 @@
 @for /f "tokens=2 delims=|" %%f in ('echo %details%') do @set isbn-10=%%f
 @for /f "tokens=3 delims=|" %%f in ('echo %details%') do @set isbn-13=%%f
 @for /f "tokens=4 delims=|" %%f in ('echo %details%') do @set edition="%%f"
-
-
 
 @set title=%title:~2,-1%
 @set publishdate=%publishdate:~105,-35%
@@ -36,6 +41,15 @@
 @echo ISBN-10 : %isbn-10%
 @echo ISBN-13 : %isbn-13%
 @echo Edition : %edition%
+@goto :cleanExit
 
+:notFoundExit
+@echo Error: No result found for %isdn%
+:cleanExit
+@rem clean varibles
+@set title=
+@set details=
+@del tempfindoutput
 @del tmpFile
+
 
