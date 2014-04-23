511e651c99327e76e3a275ae0b3e46054ee1c4ce
Alex Wu <wuyiding@gmail.com>
2014-04-23 08:44:24 +0800

    add git config setting for push.default

--- git/setup.sh
+++ git/setup.sh
@@ -2,3 +2,4 @@ git config --global user.name "alex wu"
 git config --global user.email wuyiding@gmail.com
 git config --global core.editor "vim"
 git config --global core.autocrlf true
+git config --global push.default simple
